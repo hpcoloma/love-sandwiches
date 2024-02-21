468646b6e9756947e45fcd5685252b577ddaa3c7
holly coloma <holly.life.15@gmail.com>
2024-02-21 20:15:37 +0000

    Add calcularte surplus data worksheet

--- run.py
+++ run.py
@@ -64,6 +64,15 @@ def update_sales_worksheet(data):
     print("Sales worksheet updated succesfully.\n")
     
 
+def update_surplus_worksheet(data):
+    """
+    Update surplus worksheet, add new row with the list of data provided
+    """
+    print("Updating surplus worksheet...\n")
+    surplus_worksheet = SHEET.worksheet("surplus")
+    surplus_worksheet.append_row(data)
+    print("Surplus worksheet updated succesfully.\n")
+
 
 def calculate_surplus_data(sales_row):
     """
@@ -85,11 +94,6 @@ def calculate_surplus_data(sales_row):
 
     return surplus_data
     
-
-
-
-
-
 def main():
     """
     Run all program functions
@@ -98,7 +102,7 @@ def main():
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
     new_surplus_data = calculate_surplus_data(sales_data)
-    print(new_surplus_data)
+    update_surplus_worksheet(new_surplus_data)
 
 
 print("Welcome to Love Sandwiches Data Automation")
